9358fed5d8f140370c119a47efd445bd1a783e07
Drakula <vbuzinskij@gmail.com>
2023-03-23 22:31:12 +0300

    SelectLine

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String[] SelectLine(String[] Array)
+{
+    Int32 j = 0;
+    String[] Result = new String[Array.Length];
+    String Line = String.Empty;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Line = Array[i];
+        if (Line.Length <= 3)
+        {
+            Result[j] = Line;
+            j++;
+        }
+    }
+    return Result;
+}
