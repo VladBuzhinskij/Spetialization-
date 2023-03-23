3d3c5fd394a72d32ad5c95f70662f386a32bf04d
Drakula <vbuzinskij@gmail.com>
2023-03-23 22:31:59 +0300

    DeliteNullLine

--- Program.cs
+++ Program.cs
@@ -14,3 +14,22 @@
     }
     return Result;
 }
+String[] DeleteNullLine(String[] Array)
+{
+    Int32 j = 0;
+    Int32 k = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] != null) j++;
+    }
+    String[] Result = new String[j];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] != null)
+        {
+            Result[k] = Array[i];
+            k++;
+        }
+    }
+    return Result;
+}
